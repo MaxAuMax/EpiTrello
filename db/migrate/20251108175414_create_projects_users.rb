4979989d0a348b90e88af2240aec4ae3e215f249
JeremyCMPro <jeremy.calosso-merlino@epitech.eu>
2025-11-09 22:28:23 +0100

    [~] Fix and adapt projects to handle tasks

--- db/migrate/20251108175414_create_projects_users.rb
+++ db/migrate/20251108175414_create_projects_users.rb
@@ -1,9 +1,11 @@
 class CreateProjectsUsers < ActiveRecord::Migration[8.0]
   def change
-    create_table :projects_users do |t|
-      t.belongs_to :project
-      t.belongs_to :user
-      t.timestamps
+    unless table_exists?(:projects_users)
+      create_table :projects_users do |t|
+        t.belongs_to :project
+        t.belongs_to :user
+        t.timestamps
+      end
     end
   end
 end
