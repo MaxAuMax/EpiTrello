23aa3ffb6d74977ffc7c41737d349613d0f4b8ab
JeremyCMPro <jeremy.calosso-merlino@epitech.eu>
2025-11-09 22:27:04 +0100

    [~] FIx user creation to save username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  respond_to :html, :turbo_stream
 
-  def after_sign_in_path_for(resource)
-    user_path(resource)
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 end
