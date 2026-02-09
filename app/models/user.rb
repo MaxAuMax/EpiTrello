55658c9df4b8e2ca8ba5e960bcb5f68aae940ccd
JeremyCMPro <jeremy.calosso-merlino@epitech.eu>
2026-02-09 19:35:03 +0100

    [+] Add provider to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :projects_users, dependent: :destroy
   has_many :projects, through: :projects_users
 
   validates :username, presence: true, uniqueness: true
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.username = auth.info.email.split('@').first # Génère un nom d'utilisateur depuis l'email
+    end
+  end
 end
